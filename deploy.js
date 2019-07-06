a02ab16776889206bb7d775361cea3afc3818f06
Jon Kurinsky <jkurinsky@gmail.com>
2019-07-06 10:40:45 -0500

    set up deploy folder

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+// copy all files but script.js to deploy directory
+const filesToCopy = ['index.html', 'favicon.ico', 'style.css', 'data.js'];
+filesToCopy.forEach(filename => {
+  fs.copyFile(filename, path.join('.', 'deploy', filename), err => {
+    if (err) throw err;
+  });
+});
+
+// base64 script.js and copy it in
+fs.readFile('script.js', 'UTF-8', function(err, data) {
+  if (err) throw err;
+  const base64 = Buffer.from(data).toString('base64');
+  fs.appendFile(path.join('.', 'deploy', 'script.js'), 'eval(atob("', function(
+    err
+  ) {
+    if (err) throw err;
+    fs.appendFile(path.join('.', 'deploy', 'script.js'), base64, function(err) {
+      if (err) throw err;
+      fs.appendFile(path.join('.', 'deploy', 'script.js'), '"))', function(
+        err
+      ) {
+        if (err) throw err;
+      });
+    });
+  });
+});
