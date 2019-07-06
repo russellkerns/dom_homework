eab4775b4f71993188fc59fc741a322e53a4d0db
Jon Kurinsky <jkurinsky@gmail.com>
2019-07-06 14:00:17 -0500

    add gitignore

--- deploy.js
+++ deploy.js
@@ -1,8 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
+const { exec } = require('child_process');
 
+// settings
 const deployDir = 'deploy';
+
+// make dir
+if (!fs.existsSync(path.join('.', deployDir))) {
+  fs.mkdirSync(path.join('.', deployDir));
+}
 
 // copy all project files but script.js to deploy directory
 const filesToCopy = ['index.html', 'favicon.ico', 'style.css', 'data.js'];
@@ -25,7 +32,7 @@ try {
 // convert
 const base64 = Buffer.from(plaintext).toString('base64');
 
-// save
+// save the obfuscated file
 try {
   fs.writeFileSync(path.join('.', deployDir, 'script.js'), 'eval(atob("');
 } catch (err) {
